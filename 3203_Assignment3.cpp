02f69ed6893b80feb0af0a5846ac86eb0856f4e4
DanielNino <daniel.nino@ou.edu>
2023-02-21 22:01:42 -0600

    Comments

--- 3203_Assignment3.cpp
+++ 3203_Assignment3.cpp
@@ -9,33 +9,39 @@ using namespace std;
 vector<int> list;
 
 void sum(vector<int> list){
-    int sum = 0;
+    int sum = 0; // sum of the list of numbers
 
+    //for loop to add the numbers
     for(int i = 0; i < list.size(); i++){
         sum = sum + list[i];
     }
 
+    //output sum
     cout << "sum: " << sum << endl;
 }
 
 void product(vector<int> list){
-    int prod = 0;
+    int prod = 0; // porduct of the list of numbers
 
+    //for loop to find product of list
     prod = list[0];
     for(int i = 1; i < list.size(); i++){
         prod = prod * list[i];
     }
 
+    //output product
     cout << "product: " << prod << endl;
 }
 
 int main(){
-    int temp;
+    int temp; //temporary variable to hold user inputs
 
+    //while loop for user inputted numbers
     while(cin >> temp){
         list.push_back(temp);
     }
 
+    //call functions
     sum(list);
     product(list);
 }
